9484face767bb737d389ee956decf4437e84d9cf
Maurício Grigoleto <102479476+mgrigoleto@users.noreply.github.com>
2023-03-13 16:09:27 +0000

    Alterado forma de busca de filmes, adicionado vetor para pegar os nomes dos gêneros selecionados

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var clique = document.querySelector('#sortear')
+clique.addEventListener('click', async function(){const exec = await showMovies()})
+
 async function catRandom(){
   var catURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=5bb8005de7fd8012a01a757e91ccf015&language=pt-BR"
   var catResponse = await fetch(catURL)
@@ -18,8 +21,6 @@ async function catRandom(){
     var catRandom = Math.floor(Math.random()*19)//há 19 categorias registradas no json
     var catName = catData.genres[catRandom].name
     var catID = catData.genres[catRandom].id
-    console.log(catName)
-    console.log(catID)
     if(!categorias.includes(catName)){
       categorias.push(catName)
       idCategorias.push(catID)
@@ -32,16 +33,48 @@ async function catRandom(){
 }
 
 async function showMovies(){
-  // pegar categoria
+  // pegar categorias
   var catURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=5bb8005de7fd8012a01a757e91ccf015&language=pt-BR"
   var catResponse = await fetch(catURL)
   var catData = await catResponse.json()
-  var catID = await catRandom()
+  
+  let catEscID = catRandom()
+  if(!catEscID.fulfilled){
+    catEscID = await catRandom()
+  }else{
+    // pegar o nome das categorias selecionadas anteriormente
+    var generoNome =[]
+    console.log(catEscID)
+    for(let p=0;p<19;p++){
+      if(catEscID.includes(catData.genres[p].id)){
+        generoNome.push(catData.genres[p].name)
+      }
+    }
+    console.log(generoNome)
+  }
 
+  
+
+
+  
+  var choice = document.getElementById("movFilter").value
+  
   // pegar filmes
-  var filme = "https://api.themoviedb.org/3/movie/popular?api_key=5bb8005de7fd8012a01a757e91ccf015&language=pt-BR&page=1&region=BR"
-  var filmeResponse = await fetch(filme)
-  var filmeData = await filmeResponse.json()
-  var img = filmeData.results.poster_path
-  var imgURL = "https://image.tmdb.org/t/p/w500"+img+"?api_key=5bb8005de7fd8012a01a757e91ccf015"
+  if(choice=="ppl"){
+    var filme = "https://api.themoviedb.org/3/movie/popular?api_key=5bb8005de7fd8012a01a757e91ccf015&language=pt-BR&page=1&region=BR"
+    var filmeResponse = await fetch(filme)
+    var filmeData = await filmeResponse.json()
+
+    var generoID = filmeData.results.genre_ids //pegar id do genero do filme popular
+
+    
+    var img = filmeData.results.poster_path
+    var imgURL = "https://image.tmdb.org/t/p/w500"+img+"?api_key=5bb8005de7fd8012a01a757e91ccf015" //colocar no src
+  
+    
+    
+  }
+  
+  
+  
 }
